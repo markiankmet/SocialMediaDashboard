6f7909c200e141d1cfa0894888b3ec9715d32bb1
markiiankmet <markkmet13@gmail.com>
2024-03-07 13:50:10 +0200

    add toggle logic

--- app/js/script.js
+++ app/js/script.js
@@ -1,30 +1,28 @@
-
 window.addEventListener("DOMContentLoaded", () => {
   const colorMode = localStorage.getItem("color-mode") || getComputedStyle(document.documentElement).getPropertyValue("--color-mode")
   const toggleLightButton = document.querySelector(".toggle #light")
   const toggleDarkButton = document.querySelector(".toggle #dark")
   const toggleInputs = document.querySelectorAll(`.toggle input[name="theme"]`)
-
-  if (colorMode === "dark") {
+  const setDarkMode = () => {
     document.documentElement.classList.add("dark")
+    document.documentElement.classList.remove("light")
     toggleDarkButton.checked = true
-  } else {
-    document.documentElement.classList.add("light")
-    toggleLightButton.checked = true
+    toggleLightButton.checked = false
+    localStorage.setItem("color-mode", "dark")
   }
+  const setLightMode = () => {
+    document.documentElement.classList.add("light")
+    document.documentElement.classList.remove("dark")
+    toggleLightButton.checked = true
+    toggleDarkButton.checked = false
+    localStorage.setItem("color-mode", "light")
+  }
+
+  colorMode === "dark" ? setDarkMode() : setLightMode()
 
   toggleInputs.forEach(input => {
     input.addEventListener("change", () => {
-      if (input.id === "dark") {
-        document.documentElement.classList.add("dark")
-        document.documentElement.classList.remove("light")
-        localStorage.setItem("color-mode", "dark")
-      } else {
-        document.documentElement.classList.add("light")
-        document.documentElement.classList.remove("dark")
-        localStorage.setItem("color-mode", "light")
-      }
+      input.id === "dark" ? setDarkMode() : setLightMode()
     })
   })
-
 })
